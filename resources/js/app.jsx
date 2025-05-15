ed664db2094a768aca20fc0f603a2333f96892ca
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-05-15 15:41:12 -0300

    setup

---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from './components/Example';
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Example></Example>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
